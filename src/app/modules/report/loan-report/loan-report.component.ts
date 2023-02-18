7eb5b34cb9940a3b242b9187dda50c01fcaa908d
FaysalJ2ee <faysalstat04@gmail.com>
2023-02-18 07:27:24 +0600

    loan list fixed

--- src/app/modules/report/loan-report/loan-report.component.ts
+++ src/app/modules/report/loan-report/loan-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ExcelExportService } from '../../services/excel-export.service';
 import { NotificationService } from '../../services/notification-service.service';
 import { ReportServiceService } from '../../services/report-service.service';
@@ -6,85 +7,50 @@ import { ReportServiceService } from '../../services/report-service.service';
 @Component({
   selector: 'app-loan-report',
   templateUrl: './loan-report.component.html',
-  styleUrls: ['./loan-report.component.css']
+  styleUrls: ['./loan-report.component.css'],
 })
 export class LoanReportComponent implements OnInit {
-  offset = 0;
-  length = 1000;
-  pageSize = 500;
-  pageSizeOptions: number[] = [100,500,1000];
-  transactionList:any[] = [];
-  transactionListExportable:any[] = [];
-  transactionType!:any[];
-  tnxTypes!:any[];
-  query!: any;
-
+  loanList: any[] = [];
+  loanListExportable: any[] = [];
   constructor(
     private reportService: ReportServiceService,
-    private notificationService:NotificationService,
-    private excelExportServie :ExcelExportService
-  ) { 
-    this.query = {
-      transactionType:"DEPOSIT",
-      tnxType:"",
-      transactionCategory:"LOAN",
-      fromDate:'',
-      toDate:'',
-    }
-    this.transactionType = [
-      {label:'All Category', value:''},
-      {label:'Income', value:'INCOME'},
-      {label:'Expense', value:'EXPENSE'},
-      {label:'Deposit', value:'DEPOSIT'}
-    ];
-    this.tnxTypes = [
-      {label:'All Type', value:''},
-      {label:'Debit', value:'DEBIT'},
-      {label:'Credit', value:'CREDIT'}
-    ]
-  }
+    private route: Router,
+    private excelExportServie: ExcelExportService
+  ) {}
 
   ngOnInit(): void {
-    this.fetchTransactionRecord();
+    this.fetchLoanRegistry();
   }
-  fetchTransactionRecord(){
-    const params: Map<string, any> = new Map();
-    params.set('offset',this.offset);
-    params.set('limit',this.pageSize);
-    params.set('tnxType',this.query.tnxType);
-    params.set('transactionType',this.query.transactionType);
-    params.set('fromDate',this.query.fromDate);
-    params.set('toDate',this.query.toDate);
-    params.set('transactionCategory',this.query.transactionCategory);
-    this.reportService.fetchTransactionRecord(params).subscribe({
-      next:(res)=>{
-        this.transactionListExportable = [];
-        this.transactionList= res.body.data;
-        this.length = res.body.size;
-        this.transactionList.map((elem)=>{
+  fetchLoanRegistry() {
+    this.loanListExportable = [];
+    this.reportService.fetchLoanList().subscribe({
+      next: (res) => {
+        this.loanList = res.body;
+        let index = 0;
+        this.loanList.map((elem)=>{
           let item = {
-            TNX_TYPE:elem.transactionType,
-            TNX_REASON:elem.transactionReason,
-            DEBIT: elem.income,
-            CREDIT: elem.expense,
-            TNX_DATE:elem.transactionDate,
-            REMARK:elem.refference,
+            SN: index+1,
+            CLIENT:elem.loanClient?.clientName,
+            LOAN_AMOUNT:elem.loanAmmount,
+            LOAN_BALANCE: elem.account.balance,
+            RECEIVE_DATE: elem.receiveDate,
+            RETURN_DATE:elem.returnDate,
+            REMARK:elem.remark,
           };
-          this.transactionListExportable.push(item);
+          index++;
+          this.loanListExportable.push(item);
         })
       },
-      error:(err)=>{
-        console.log(err.message)
-      }
-    })
-  }
-  pageChange(event:any){
-    this.pageSize = event.pageSize;
-    this.offset = this.pageSize * event.pageIndex;
-    this.fetchTransactionRecord();
-  }
-  exportAsExcel(){
-      this.excelExportServie.exportAsExcelFile(this.transactionListExportable,"Tnx-Report");
+    });
   }
 
+  exportAsExcel() {
+    this.excelExportServie.exportAsExcelFile(
+      this.loanListExportable,
+      'Loan-Report'
+    );
+  }
+  viewDetails(id:any){
+    this.route.navigate(["cash/loan-details",id]);
+  }
 }
